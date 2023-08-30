22219afc4047c60d6ef307bf4c3e92c40619c47c
Jorge <jorlopeztohme@gmail.com>
2023-08-30 00:06:58 -0300

    Add table put

--- routers/tables-router.js
+++ routers/tables-router.js
@@ -12,4 +12,17 @@ tablesRouter.get('/', async (req, res) => {
   else res.status(200).json(tables)
 })
 
+tablesRouter.put('/open/:tableId', async (req, res) => {
+  const { tableId } = req.params
+  const { people } = req.body
+  console.log(req.body)
+  const { data: table, error } = await supabase
+    .from('table')
+    .update({ state: 'busy', people })
+    .eq('id', tableId)
+
+  if (error) res.status(400).json({ error: error.message })
+  else res.status(200).json(table)
+})
+
 export default tablesRouter
