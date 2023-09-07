075e012fabcd22a997dcc748d96a7d491bd6208c
Jorge <jorlopeztohme@gmail.com>
2023-09-07 16:30:40 -0300

    [wip]

--- routers/orders-router.js
+++ routers/orders-router.js
@@ -7,8 +7,10 @@ ordersRouter.get('/today', async (req, res) => {
   const { data: todayOrders, error } = await supabase
     .from('order')
     .select('*')
-    .order('created_at', { ascending: true })
-    // .eq('created_at', new Date().toISOString().slice(0, 10))
+    // get order from the last 12 hours and finished: false
+    .gte('created_at', new Date(Date.now() - 12 * 60 * 60 * 1000).toISOString())
+    .eq('finished', false)
+    .order('created_at', { ascending: false })
 
   // get items from the order
   const { data: orderItem, error2 } = await supabase
@@ -43,19 +45,17 @@ ordersRouter.post('/', async (req, res) => {
   // create table order
   const { data: tableOrder, error } = await supabase
     .from('order')
-    .insert({ waiter_name: data.waiterName, table: data.tableNumber })
+    .insert({ waiter_name: data.waiterName, table_id: data.tableNumber })
     .single()
     .select()
 
   // create order items
-  // tableOrder is undefined
   const orderItems = data.orderItems.map((item) => {
     return {
       order_id: tableOrder.id,
       item_id: item.id,
       note: item.note,
       quantity: item.quantity
-
     }
   })
 
@@ -65,6 +65,20 @@ ordersRouter.post('/', async (req, res) => {
 
   if (error || error2) res.status(400).json({ error: error.message })
   else res.status(200).json({ order: tableOrder, orderItems: orderItemsData })
+})
+
+ordersRouter.put('/:id', async (req, res) => {
+  const { id } = req.params
+  const { status } = req.body
+
+  const { data, error } = await supabase
+    .from('order')
+    .update({ status })
+    .eq('id', id)
+    .single()
+
+  if (error) res.status(400).json({ error: error.message })
+  else res.status(200).json(data)
 })
 
 export default ordersRouter
